36a62658732d0162b7b76d53e75c413b166f9de0
Anveks <vekselmana@gmail.com>
2023-07-01 10:48:57 +0300

    .

--- games/hangman/hangman-game.py
+++ games/hangman/hangman-game.py
@@ -2,17 +2,20 @@ import random
 import words
 import logo
 
-print(logo.logo)
+print(logo.logo) # adding a logo
 
-selected_word = words.word_list[random.randint(0, len(words.word_list) - 1)]
-blank_word = []
-tries = 6
+selected_word = words.word_list[random.randint(0, len(words.word_list) - 1)] # picking a random word
+blank_word = [] # preparing an empty list
+tries = 6 # number of tries is standard
 
-for e in range(len(selected_word)):
+for _ in range(len(selected_word)): # filling the blank list with dashes of the same number with selected word
     blank_word.append('_')
 
-while tries != 0 and blank_word.count('_') != 0:
+while tries != 0 and blank_word.count('_') != 0: # defining the condition: untill tries are not zero and there are still dashes in the blank_word
     guess = input('Guess a letter: \n').lower()
+
+    if guess in blank_word:
+        print(f'You have already guessed this letter.')
 
     for i in range(0, len(selected_word)):
         letter = selected_word[i]
