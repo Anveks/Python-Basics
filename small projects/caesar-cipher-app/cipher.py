56c3c7544bcf1d690bebb13b2c5097499bdd8ddb
Anveks <vekselmana@gmail.com>
2023-07-03 12:50:06 +0300

    .

--- small projects/caesar-cipher-app/cipher.py
+++ small projects/caesar-cipher-app/cipher.py
@@ -1,28 +1,43 @@
+import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(logo.logo)
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+alphabet_length = len(alphabet)
 
 def encrypt(user_text, user_shift):
-    alphabet_length = int(len(alphabet))
     encrypted_text = ""
 
-    for letter in user_text: 
-      index = int(alphabet.index(letter))
-      
-      if direction == 'encode':
-        new_index = index + user_shift
-        if (new_index > alphabet_length): new_index = user_shift - (alphabet_length - index)
+    for letter in user_text:  # Looping through the whole text
+        
+        if letter == ' ':  # Check for space character
+            encrypted_text += ' '  # Append spaces as is
+            continue  # Move to the next iteration
 
-      else:
-        new_index = index - user_shift
-        if (new_index < 0): new_index = abs(new_index)
+        if letter not in alphabet:
+            encrypted_text += letter
+            continue
 
-      letter = alphabet[new_index]
-      encrypted_text += letter
+        index = alphabet.index(letter)  # Getting index of each letter
 
-    print(encrypted_text)     
+        if direction == 'encode':
+            new_index = (index + user_shift) % alphabet_length # modulo will give the remainder
+        elif direction == 'decode':
+            new_index = (index - user_shift) % alphabet_length
+        else:
+            print('Something went wrong... Check all the inputs and try again.')
+            return
 
-encrypt(text, shift)
+        letter = alphabet[new_index]
+        encrypted_text += letter
+
+    print(f'The result is: {encrypted_text}')
+
+if shift > alphabet_length:
+    print('The shift cannot exceed the alphabet length!')
+else:
+    encrypt(text, shift)
